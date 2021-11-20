c9219fd7bfab550ce7bf48f4d43e7781819c088e
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-20 18:09:02 +0100

    refactor: moved product service to new path

--- src/service/ProductService/addToCart.service.js
+++ src/service/ProductService/addToCart.service.js
@@ -1,4 +1,4 @@
-import apiClient from '../infrastructure/api/apiClient';
+import apiClient from '../../infrastructure/api/apiClient';
 
 const getProduct = async (product) => apiClient.postProductToCart(product);
 
