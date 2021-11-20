84be371ffa657109605fb22f1235c0b084b81635
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-20 17:27:24 +0100

    test: added api client test and products fixtures

---
+++ src/api/apiClient.test.js
@@ -0,0 +1,25 @@
+import { allProducts } from '../../fixtures/allProducts.fixture';
+import { singleProduct } from '../../fixtures/singleProduct.fixture';
+import apiClient from './apiClient';
+
+describe('apiClient', () => {
+  test('should return an object with all items', async () => {
+    const { data: products } = await apiClient.getProducts();
+    expect(products).toMatchObject(allProducts);
+  });
+
+  test('should return an object with item detail by id', async () => {
+    const productId = singleProduct.id;
+    const { data: product } = await apiClient.getProduct(productId);
+    expect(product).toMatchObject(singleProduct);
+  });
+  test('should add an object to cart', async () => {
+    const productToAdd = {
+      id: 'ZmGrkLRPXOTpxsU4jjAcv',
+      colorCode: 1000,
+      storageCode: 2000,
+    };
+    const { data: count } = await apiClient.postProductToCart(productToAdd);
+    expect(count).toMatchObject({ count: 1 });
+  });
+});
