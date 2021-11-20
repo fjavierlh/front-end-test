86d3947eff697495f9a8cbdcc21e26169fa37a8d
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-20 17:47:51 +0100

    chore: created storage to store api data

---
+++ src/infrastructure/storage/storage.js
@@ -0,0 +1,25 @@
+const storage = {
+  get(key) {
+    return localStorage.getItem(key);
+  },
+
+  set(key, value) {
+    const now = new Date();
+
+    const item = {
+      value,
+      expires: now.getTime() + 60 * 60 * 1000, // 1 hour
+    };
+    localStorage.setItem(key, item);
+  },
+
+  remove(key) {
+    localStorage.removeItem(key);
+  },
+
+  clear() {
+    localStorage.clear();
+  },
+};
+
+export default storage;
