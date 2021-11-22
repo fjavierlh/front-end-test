f09af3fbb73f46ce774dc285d633a8bfa078ca55
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-22 17:23:42 +0100

    test: mocked api client server call

--- src/infrastructure/api/apiClient.test.js
+++ src/infrastructure/api/apiClient.test.js
@@ -1,19 +1,30 @@
 import { allProducts } from '../../../fixtures/allProducts.fixture';
 import { singleProduct } from '../../../fixtures/singleProduct.fixture';
+import axios from '../../config/axios.config';
 import apiClient from './apiClient';
+
+jest.mock('../../config/axios.config');
 
 describe('apiClient', () => {
   test('should return an object with all items', async () => {
+    axios.get.mockImplementation(() => Promise.resolve({ data: allProducts }));
     const { data: products } = await apiClient.getAllProducts();
     expect(products).toMatchObject(allProducts);
   });
 
   test('should return an object with item detail by id', async () => {
+    axios.get.mockImplementation(() =>
+      Promise.resolve({ data: singleProduct })
+    );
+
     const productId = singleProduct.id;
     const { data: product } = await apiClient.getProduct(productId);
     expect(product).toMatchObject(singleProduct);
   });
   test('should add an object to cart', async () => {
+    axios.post.mockImplementation(() =>
+      Promise.resolve({ data: { count: 1 } })
+    );
     const productToAdd = {
       id: 'ZmGrkLRPXOTpxsU4jjAcv',
       colorCode: 1000,
