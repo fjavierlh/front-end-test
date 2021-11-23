c2cdff6dfffd52603da146f8a1fc23e7927afd57
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-23 16:16:21 +0100

    chore: created main layout component

---
+++ src/layout/MainLayout.js
@@ -0,0 +1,9 @@
+import styled from 'styled-components';
+
+const MainLayout = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #efefef;
+`;
+
+export default MainLayout;
