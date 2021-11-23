7e9ecc05db604cd56a7c3fe65752812f78d8a7e5
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-23 16:21:01 +0100

    feat: added router to app and custom hooks to add main functionality

--- src/App.js
+++ src/App.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
+import Header from './components/Header';
+import useAddProductToCart from './hooks/useAddProductToCart.hook';
+import useFetchAllProducts from './hooks/useFetchAllProducts.hook';
+import MainLayout from './layout/MainLayout';
+import ListProductViewRaw from './views/ListProductView';
+import { PageNotFoundView } from './views/PageNotFoundView';
+import { ProductDetailView } from './views/ProductDetail';
 
 const App = () => {
+  const { products, ...loadStateProducts } = useFetchAllProducts();
+  const { cartCount, addProductToCart } = useAddProductToCart();
+
   return (
     <>
-      <h1>Mobile devices shop</h1>
+      <MainLayout>
+        <Header title="Mobile devices shop" cartCount={cartCount} />
+        <Routes>
+          <Route
+            exact
+            path="/"
+            element={
+              <ListProductViewRaw
+                products={products}
+                loadState={loadStateProducts}
+              />
+            }
+          />
+          <Route
+            path="/product/:model"
+            element={<ProductDetailView onAddToCart={addProductToCart} />}
+          />
+          <Route path="*" element={<PageNotFoundView />} />
+        </Routes>
+      </MainLayout>
     </>
   );
 };
