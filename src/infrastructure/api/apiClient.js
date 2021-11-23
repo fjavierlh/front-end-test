050018bafc4ff1a0d6122c5f690eff3171c68d56
Javier Lopez Herrera <fjlopez.contractor@veridas.com>
2021-11-23 16:09:21 +0100

    refactor: removed unnecessary awaits

--- src/infrastructure/api/apiClient.js
+++ src/infrastructure/api/apiClient.js
@@ -1,11 +1,11 @@
 import axios from '../../config/axios.config';
 
 const apiClient = {
-  getAllProducts: async () => await axios.get('/product'),
+  getAllProducts: async () => axios.get('/product'),
 
-  getProduct: async (id) => await axios.get(`/product/${id}`),
+  getProduct: async (id) => axios.get(`/product/${id}`),
 
-  postProductToCart: async (product) => await axios.post('/cart', product),
+  postProductToCart: async (product) => axios.post('/cart', product),
 };
 
 export default apiClient;
