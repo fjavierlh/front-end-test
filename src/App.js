b97579945103a944346550bb40cf2321bbf18a41
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-11-24 11:20:27 +0100

    refactor: moved main layout into router

--- src/App.js
+++ src/App.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { Route, Routes, useLocation } from 'react-router-dom';
-import { BreadCrumb } from './components/BreadCrumb';
-import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
 import useAddProductToCart from './hooks/useAddProductToCart.hook';
 import useFetchAllProducts from './hooks/useFetchAllProducts.hook';
 import MainLayout from './layout/MainLayout';
@@ -10,33 +8,27 @@ import { PageNotFoundView } from './views/PageNotFoundView';
 import { ProductDetailView } from './views/ProductDetail';
 
 const App = () => {
-  const location = useLocation();
   const { products, ...loadStateProducts } = useFetchAllProducts();
   const { cartCount, addProductToCart } = useAddProductToCart();
   return (
-    <>
-      <MainLayout>
-        <Header title="Mobile devices shop" cartCount={cartCount} />
-        <BreadCrumb location={location} />
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={
-              <ListProductViewRaw
-                products={products}
-                loadState={loadStateProducts}
-              />
-            }
-          />
-          <Route
-            path="/product/:model"
-            element={<ProductDetailView onAddToCart={addProductToCart} />}
-          />
-          <Route path="*" element={<PageNotFoundView />} />
-        </Routes>
-      </MainLayout>
-    </>
+    <Routes>
+      <Route path="/" element={<MainLayout cartCount={cartCount} />}>
+        <Route
+          index
+          element={
+            <ListProductViewRaw
+              products={products}
+              loadState={loadStateProducts}
+            />
+          }
+        />
+        <Route
+          path="/product/:model"
+          element={<ProductDetailView onAddToCart={addProductToCart} />}
+        />
+        <Route path="*" element={<PageNotFoundView />} />
+      </Route>
+    </Routes>
   );
 };
 
--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -1,9 +1,27 @@
+import React from 'react';
+import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
+import { BreadCrumb } from '../components/BreadCrumb';
+import Header from '../components/Header';
+import breakpoints from '../constants/devices-sizes';
 
-const MainLayout = styled.div`
+export const MainLayout = ({ cartCount }) => {
+  return (
+    <MainLayoutStyled>
+      <Header title="Mobile devices shop" cartCount={cartCount} />
+      <BreadCrumb />
+      <Outlet />
+    </MainLayoutStyled>
+  );
+};
+
+const MainLayoutStyled = styled.div`
   display: flex;
   flex-direction: column;
   background-color: #efefef;
+  max-width: ${breakpoints.size.lg};
+  margin: 0 auto;
+  padding: 1rem;
 `;
 
 export default MainLayout;
