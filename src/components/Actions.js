4028d798f99c70061e7836e2af5814057c2c6bc5
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-11-24 14:50:19 +0100

    feat: added styles to actions and button component

--- src/components/Actions.js
+++ src/components/Actions.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import styled from 'styled-components';
 import { AddToCartButton } from './AddToCartButton';
 
-export const Actions = ({ id, onSubmit, onChangeColor, onChangeStorage, options, userOptions }) => {
+export const Actions = ({
+  id,
+  onSubmit,
+  onChangeColor,
+  onChangeStorage,
+  options,
+  userOptions,
+}) => {
   const { colors, storages } = options;
   return (
-    <div>
-      Options:
+    <ActionsStyled>
+      <h3>Options</h3>
       <form onSubmit={onSubmit}>
         <select onChange={onChangeColor}>
           <option key={`${id}-colors`} disabled selected>
@@ -31,6 +39,23 @@ export const Actions = ({ id, onSubmit, onChangeColor, onChangeStorage, options,
         </select>
         <AddToCartButton userOptions={userOptions} text="Add to cart" />
       </form>
-    </div>
+    </ActionsStyled>
   );
 };
+
+const ActionsStyled = styled.div`
+  & > form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+  }
+  & > form > select {
+    font-size: 1.2rem;
+    display: block;
+    width: 320px;
+    border: none;
+    background-color: #fff;
+    margin: 1rem auto;
+    padding: 0.5rem;
+  }
+`;
--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,21 @@
 import React from 'react';
+import styled from 'styled-components';
 
 export const Button = ({ text, children, disabled }) => {
-  return <button disabled={disabled}>{text ? text : children}</button>;
+  return (
+    <ButtonStyled disabled={disabled}>{text ? text : children}</ButtonStyled>
+  );
 };
+
+const ButtonStyled = styled.button`
+  background-color: #4caf50;
+  border: none;
+  color: white;
+  padding: 15px 32px;
+  text-align: center;
+  text-decoration: none;
+  display: inline-block;
+  font-size: 16px;
+  margin: 4px 2px;
+  cursor: pointer;
+`;
