5f61dffdb3d3489a25a63574540bc459e0c57431
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-11-24 16:58:12 +0100

    feat: added styles to button when is disabled

--- src/components/Button.js
+++ src/components/Button.js
@@ -11,6 +11,7 @@ const ButtonStyled = styled.button`
   background-color: #4caf50;
   border: none;
   color: white;
+  font-weight: bold;
   padding: 15px 32px;
   text-align: center;
   text-decoration: none;
@@ -18,4 +19,10 @@ const ButtonStyled = styled.button`
   font-size: 16px;
   margin: 4px 2px;
   cursor: pointer;
+
+  &:disabled {
+    background-color: #555;
+    color: #ccc;
+    cursor: not-allowed;
+  }
 `;
