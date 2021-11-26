f92987c8f6226a1d6b9190c7f267f4e19cace3ae
F Javier López Herrera <franciscojavier.lopez@kairosds.com>
2021-11-26 15:39:45 +0100

    test: add more use cases to search bar component test suite

--- src/__test__/components/SearchBar.test.js
+++ src/__test__/components/SearchBar.test.js
@@ -1,10 +1,38 @@
-import { render } from '@testing-library/react';
+import { act, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { allProducts } from '../../../fixtures/allProducts.fixture';
 import SearchBar from '../../components/SearchBar';
 
+const onSearchMock = jest.fn();
+
 describe('SearchBar', () => {
-  const onSearchMock = jest.fn();
   test('should render SearchBar component', () => {
+    const { container } = render(
+      <SearchBar onSearch={onSearchMock} products={allProducts} />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('should render search input with placeholder text', () => {
     render(<SearchBar onSearch={onSearchMock} products={allProducts} />);
+    expect(
+      screen.queryByPlaceholderText(/search model or brand/i)
+    ).toBeInTheDocument();
+  });
+
+  test('should render search input and allow user to type', () => {
+    render(<SearchBar onSearch={onSearchMock} products={allProducts} />);
+    const searchInput = screen.getByPlaceholderText(/search model or brand/i);
+    expect(searchInput).toBeInTheDocument();
+
+    act(() => {
+      userEvent.click(searchInput);
+    });
+
+    expect(searchInput).toHaveFocus();
+    expect(searchInput).toHaveValue('');
+
+    userEvent.type(searchInput, 'test');
+    expect(searchInput.value).toBe('test');
   });
 });
